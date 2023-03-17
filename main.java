c09e12d5a20a7d9ca234ce54b00b756f4cbe1173
dialga4ever <dialga4ever@gmail.com>
2023-03-17 14:31:44 +0100

    truc

--- main.java
+++ main.java
@@ -1,18 +1,29 @@
 public class main {
     public static void testTabNote(int tab[]){
+        
         try{
             for(int i=0;i<tab.length;i++){
+                
                 if(tab[i]<0){
                     throw new GradeException(("note inferieur a 0 indice:"+i+" valeur :"+tab[i]));
+                    
                 }
                 if(tab[i]>20){
+                    
                     throw new GradeException(("note superieur a 20 indice:"+i+" valeur :"+tab[i]));
                 }
             }
         }catch(GradeException probleme){
             System.out.println(probleme);
         }
-        
+        finally{
+            if(){
+                System.out.println("Toutes les notes sont correctes");
+            }
+            else{
+                System.out.println("Des notes incorrectes ont ´et´e trouv´ees");
+            }
+        }
 
     }
     public static void main(String[] args) {
